c104ad2370ee91e66d49533f44244a2112057c16
kaushiki <kaushiki.ak88@gmail.com>
2021-04-15 17:34:17 +0530

    ADDED: testcase to ensure number of records

--- Src/StateCensusData.js
+++ Src/StateCensusData.js
@@ -1,16 +1,28 @@
 const csvtoJson = require('csvtojson');
+const fs = require('fs');
+const csv = require('csv-parser');
+var array = [];
 
 class CensusAnalyser {
-    loadCsvData(path, callback) {
-        let count;
-        csvtoJson()
-        .fromFile(path)
-        .then((JSON) => {
-            console.log(JSON);
-            count = Object.keys(JSON).length;
-            return callback(count);
-        });
-    }
+  loadCsvData(path, callback) {
+    fs.createReadStream(path)
+      .pipe(csv())
+      .on(("data"), (row) => {
+        array.push(row);
+        console.log("data : "+JSON.stringify(row))
+      })
+      .on("end", () => {
+        callback(array.length)
+        console.log('CSV file successfully processed');
+      });
+  } 
+  csvToJsonConversion(path, callback) {
+    fs.createReadStream(path)
+    .pipe(csv())
+    .on('end',() => {
+      callback(path)
+    })
+  }
 }
 
 module.exports = CensusAnalyser;
--- Test/StateCenesusData.test.js
+++ Test/StateCenesusData.test.js
@@ -1,12 +1,18 @@
 const assert = require("chai").assert;
 const censusAnalyser = require("../Src/StateCensusData");
-const INDIA_STATE_CENSUS_FILE_PATH = "./resources/IndiaStateCensusData.csv";
+const STATE_CENSUS_FILE_PATH = './resources/StateCensusData.csv';
 
 var CensusAnalyser = new censusAnalyser();
+
 describe("IndiaStateCensusAnalyser", function () {
   it("Loads the number of records 29 from csv file", function () {
-    CensusAnalyser.loadCsvData(INDIA_STATE_CENSUS_FILE_PATH, function (count) {
-      assert.equal(count, 29);
+    CensusAnalyser.loadCsvData(STATE_CENSUS_FILE_PATH, function (row) {
+      assert.equal(row, 29);
     });
   });
+  it("Given wrong INDIA_STATE_CENSUS_FILE_PATH", function () {
+    CensusAnalyser.loadCsvData(STATE_CENSUS_FILE_PATH, function (row) {
+        assert.notEqual(row, 29);
+    });
+});
 });
